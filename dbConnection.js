86aa457302a22a666cc6b3295766ff5ba4d7a583
AbdulRehmanSethi <abdulrehmansethi3@gmail.com>
2025-03-23 23:14:45 +0500

    Updated the connection string.

--- dbConnection.js
+++ dbConnection.js
@@ -1,27 +1,24 @@
-
-
 // dbConnect();
-require('dotenv').config();
+require("dotenv").config();
 const { mongoose } = require("mongoose");
 
 const dbConnect = async () => {
-    const username = encodeURIComponent(process.env.MONGO_USERNAME);
-    const password = encodeURIComponent(process.env.MONGO_PASSWORD);
-    const host = process.env.MONGO_HOST;
-    const dbName = process.env.MONGO_DB_NAME;
-    const options = process.env.MONGO_OPTIONS;
+  const username = encodeURIComponent(process.env.MONGO_USERNAME);
+  const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+  const host = process.env.MONGO_HOST;
+  const dbName = process.env.MONGO_DB_NAME;
+  const options = process.env.MONGO_OPTIONS;
 
-    const uri = `mongodb+srv://${username}:${password}@${host}/${dbName}?${options}`;
+  const uri = `mongodb://${username}:${password}@${host}`;
+  try {
+    await mongoose.connect(uri, {
+      serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds
+    });
 
-    try {
-        await mongoose.connect(uri, {
-            serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds
-        });
-
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("Database connection error:", error);
-    }
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
 };
 
 dbConnect();
